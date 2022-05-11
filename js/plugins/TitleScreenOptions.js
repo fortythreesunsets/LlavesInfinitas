eddd174ec19ad55ce6cbde471dacab93bded2cad
s9r <sruv_2601@hotmail.com>
2022-05-11 16:40:29 -0500

    mostrar controles
    Muestra controles antes de empezar
    Elimina Opciones del menú principal
    Eliminación de imágenes no utilizadas

--- js/plugins/TitleScreenOptions.js
+++ js/plugins/TitleScreenOptions.js
@@ -1,7 +1,7 @@
 Window_TitleCommand.prototype.makeCommandList = function() {
     this.addCommand(TextManager.newGame,   'newGame');
     //this.addCommand(TextManager.continue_, 'continue', this.isContinueEnabled());
-    this.addCommand(TextManager.options,   'options');
+    //this.addCommand(TextManager.options,   'options');
 };
 
 Window_MenuCommand.prototype.addOptionsCommand = function() {
